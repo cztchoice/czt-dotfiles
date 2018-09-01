dad23145bf1c4265bcb41f0851e109944e758685
jiangsuo <jiangsuo@pinduoduo.com>
2018-09-01 22:05:39 +0800

    增加tmux配置

--- init_centos_7.sh
+++ init_centos_7.sh
@@ -10,7 +10,7 @@ sudo yum install -y zsh
 # 配置支持：appimage
 sudo yum --enablerepo=epel -y install fuse-sshfs
 user="$(whoami)"
-sudo usermod -a -G fuse "$user"
+# sudo usermod -a -G fuse "$user"
 
 curl -fLo ~/bin/nvim --create-dirs https://gitee.com/cztchoice/czt-dotfiles/raw/master/nvim.appimage
 chmod u+x ~/bin/nvim
@@ -18,7 +18,7 @@ chmod u+x ~/bin/nvim
 sudo yum install the_silver_searcher -y
 sudo yum install python36-setuptools -y
 sudo easy_install-3.6 pip
-sudo pip3 install neovim
+sudo /usr/local/bin/pip3 install neovim
 
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -27,3 +27,10 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+cat >> ~/.bashrc <<EOF
+tmuxjiangsuo() {
+   tmux attach -t jiangsuo || tmux -f .tmux_jiangsuo.conf new -s jiangsuo
+}
+EOF
